f884000b3dfac71f25bf2d665ae3055ef5a71d62
RAtioNAn <rationan@fastmail.com>
2025-05-03 00:15:40 +0300

    WIP

--- backend/query/build.gradle.kts
+++ backend/query/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("com.clickhouse:clickhouse-jdbc:0.6.5")
     implementation("io.github.pelenthium:clickhouse-dialect-spring-boot-starter:1.2.0")
     implementation("org.apache.httpcomponents.client5:httpclient5")
